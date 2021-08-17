6058d194630aca64509e6ae587fab80667bf51ca
hamza887 <hamzabutt887@hotmail.com>
2021-08-17 05:38:46 -0700

    working on Header

--- allinone/src/App.js
+++ allinone/src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Header/>
     </div>
   );
 }
---
+++ allinone/src/components/Header/Header.js
@@ -0,0 +1,5 @@
+export default function Header(){
+    return (
+        <h1>hello form header</h1>
+    )
+}
