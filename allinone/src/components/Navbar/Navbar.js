8b828d34fd94d2d7315092ecb62735d976749f00
hamza887 <hamzabutt887@hotmail.com>
2021-08-17 04:45:31 -0700

    adding bootstrap in Navbar

--- allinone/src/components/Navbar/Navbar.js
+++ allinone/src/components/Navbar/Navbar.js
@@ -1,8 +1,24 @@
 export default function Navbar(){
     return (
-        <>
-        <h2>hello</h2>
-        <button className='btn btn-danger' >helodfd</button>
-        </>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#">ALL IN ONE</a>
+      
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">link 2</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     )
 }
