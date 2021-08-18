f646fcb9d36be6aeec9e1f1dadd01c67042e6f7b
hamza887 <hamzabutt887@hotmail.com>
2021-08-18 01:42:16 -0700

    still working on the books component

--- allinone/src/components/Books/Books.js
+++ allinone/src/components/Books/Books.js
@@ -1,14 +1,38 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import axios from 'axios';
 export default function Books(){
-  
+  const[booksearch, setBooksearch]=useState('');
+  const[searchresult,setSearchresult]=useState([]);
 
-  axios.get(`https://www.googleapis.com/books/v1/volumes?q=java&key=AIzaSyAFH6VcFGFwnoVaO-ER32twGdgCa86v8Dw&maxResults=20`).then(hh=>{
-    console.log(hh.data.items);
+  
+  const SearchSubmit=(e)=>{
+     const mybook= e.target.value;
+     setBooksearch(mybook);
+}
+
+const formSearchSubmit=(e)=>{
+  e.preventDefault()
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${booksearch}&key=AIzaSyAFH6VcFGFwnoVaO-ER32twGdgCa86v8Dw&maxResults=20`)
+  .then(hh =>{
+      console.log(hh.data.items);
+      setSearchresult(hh.data.items)
   })
+}
 
 
     return(
-        <h1>hello</h1>
+        <div className='container'>
+          <h2>hello thios is a book search option</h2>
+          <p>Search for any FAV book you like and download and read the PDF</p>
+          <form onSubmit={formSearchSubmit}>
+            <div>
+              <input type='text' onChange={SearchSubmit} placeholder='search books'/>
+            </div>
+            <button type='submit' className='btn btn-danger'>Search books</button>
+          </form>
+          <div>
+
+          </div>
+        </div>
     )
 }
