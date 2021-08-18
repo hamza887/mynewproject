c2b84f9dff05a5ba4928afdda9f55daffc1e6587
hamza887 <hamzabutt887@hotmail.com>
2021-08-17 17:18:50 -0700

    currently working on header

--- allinone/src/components/Header/Header.js
+++ allinone/src/components/Header/Header.js
@@ -1,5 +1,10 @@
 export default function Header(){
     return (
-        <h1>hello form header</h1>
+        <div className='header-head'>
+        <div className='header-text'>
+        <h1 className='myheading'>All Of Your FAV Apps </h1>
+        <h1 className='myheading'>At One Place</h1>
+        </div>
+        </div>
     )
 }
