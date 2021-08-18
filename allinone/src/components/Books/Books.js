82cf727b1246816932747da9ec14f6b938c8084c
hamza887 <hamzabutt887@hotmail.com>
2021-08-18 00:52:21 -0700

    console log checking the data from api\

--- allinone/src/components/Books/Books.js
+++ allinone/src/components/Books/Books.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 export default function Books(){
+  
+
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=java&key=AIzaSyAFH6VcFGFwnoVaO-ER32twGdgCa86v8Dw&maxResults=20`).then(hh=>{
+    console.log(hh.data.items);
+  })
+
+
     return(
-        <div className="input-group">
-        <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search"
-          aria-describedby="search-addon" />
-        <button type="button" className="btn btn-outline-primary">search</button>
-      </div>
+        <h1>hello</h1>
     )
 }
