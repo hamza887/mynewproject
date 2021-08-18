43209f0af7bb487e82f08a61715744c56298438f
hamza887 <hamzabutt887@hotmail.com>
2021-08-17 22:02:31 -0700

    working on a new component

--- allinone/src/App.js
+++ allinone/src/App.js
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Books from './components/Books/Books'
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Header/>
+     <Books/>
     </div>
   );
 }
